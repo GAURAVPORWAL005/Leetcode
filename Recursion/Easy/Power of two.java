0b2e811e2a8c0ef76d9e71ef96746b20d2f92c41
Gaurav Porwal <53477729+GAURAVPORWAL005@users.noreply.github.com>
2022-06-05 19:17:02 +0530

    Create Power of two.java

---
+++ Recursion/Easy/Power of two.java
@@ -0,0 +1,17 @@
+// Question => https://leetcode.com/problems/power-of-two/submissions/
+
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        if(n<=0){
+            return false;
+        }
+     if(n%2==0){
+       return isPowerOfTwo(n/2);
+     }
+           if(n == 1){
+            return true;
+        }
+        return false;
+        
+    }
+}
