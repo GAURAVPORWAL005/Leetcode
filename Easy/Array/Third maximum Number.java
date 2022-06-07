9a6a59d78e933a4c9b5d367d42911d538561b8d1
Gaurav Porwal <53477729+GAURAVPORWAL005@users.noreply.github.com>
2022-06-07 18:26:50 +0530

    Create Third maximum Number.java

---
+++ Easy/Array/Third maximum Number.java
@@ -0,0 +1,29 @@
+// Question => https://leetcode.com/problems/third-maximum-number/submissions/
+class Solution {
+    public int thirdMax(int[] nums) {
+     int max=0;
+        int count=0;
+        
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        
+        Arrays.sort(nums);
+        for(int i=nums.length-1;i>=0;i--)
+        {
+          if(!list.contains(nums[i]))
+              {
+                  list.add(nums[i]);
+                  count++;
+                  max=Math.max(max,nums[i]);  
+              }
+              
+              if(count==3)
+              {
+                  return nums[i];   
+              } 
+        }
+        
+        return max;
+    }
+}
+
+// Time Complexity => O(n).
