2decd9e27589f9cffb7594221100f17a35b07dc4
Gaurav Porwal <53477729+GAURAVPORWAL005@users.noreply.github.com>
2022-06-07 12:37:49 +0530

    Create Build Array from permutation.java

---
+++ Easy/Array/Build Array from permutation.java
@@ -0,0 +1,13 @@
+// Question => https://leetcode.com/problems/build-array-from-permutation/submissions/
+class Solution {
+    public int[] buildArray(int[] nums) {
+        int n = nums.length;
+        int[] ans = new int[n];
+        for(int i=0;i<n;i++){
+            ans[i] = nums[nums[i]];
+        }
+        return ans;
+    }
+}
+
+// Time Complexity => O(n)
