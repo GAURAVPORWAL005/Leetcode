bf3801846aefc8a1fc4e81a191e4b56341fc0f92
Gaurav Porwal <53477729+GAURAVPORWAL005@users.noreply.github.com>
2022-06-10 10:28:45 +0530

    Update Matrix Diagonal sum.java

--- Easy/Array/Matrix Diagonal sum.java
+++ Easy/Array/Matrix Diagonal sum.java
@@ -1,4 +1,26 @@
 // Question => https://leetcode.com/problems/matrix-diagonal-sum/submissions/
+
+// Brute Force Approach
+class Solution {
+    public int diagonalSum(int[][] mat) {
+        int sum = 0;
+        int n = mat.length;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(i == j){
+                    sum += mat[i][j];
+                }
+                else if(i + j == n-1){
+                    sum += mat[i][j];
+                }
+            }
+        }
+        return sum;
+    }
+}
+
+// Time complexity => O(n^2)-
+
 // Optimized Approach
 class Solution {
     public int diagonalSum(int[][] matrix) {
