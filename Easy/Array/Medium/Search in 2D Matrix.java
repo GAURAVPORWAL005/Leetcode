bfe7792fd1ee7e8d12601e9d2b12dd40b5082f41
Gaurav Porwal <53477729+GAURAVPORWAL005@users.noreply.github.com>
2022-06-13 01:29:31 +0530

    Create Search in 2D Matrix.java

---
+++ Easy/Array/Medium/Search in 2D Matrix.java
@@ -0,0 +1,37 @@
+// Question => https://leetcode.com/problems/search-a-2d-matrix/submissions/
+
+// Brute force Approach
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[0].length;j++){
+                if(matrix[i][j] == target){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
+// Time complexity => O(n^2)
+
+// Optimized Approach
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int i = 0; int j = matrix[0].length-1;
+        while(i < matrix.length && j >= 0){
+            if(matrix[i][j] == target){
+                return true;
+            }
+            if(target < matrix[i][j]){
+                j--;
+            }
+            else{
+                i++;
+            }
+        }
+        return false;
+    }
+}
+
+// Time complexity => O(n)
