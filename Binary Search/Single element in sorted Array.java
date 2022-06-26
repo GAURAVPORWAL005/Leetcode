f36dc911431dddcfde5065e93d5213c9436b98cf
Gaurav Porwal <53477729+GAURAVPORWAL005@users.noreply.github.com>
2022-06-27 00:18:04 +0530

    Update Single element in sorted Array.java

--- Binary Search/Single element in sorted Array.java
+++ Binary Search/Single element in sorted Array.java
@@ -1,5 +1,5 @@
 // Question => https://leetcode.com/problems/single-element-in-a-sorted-array/
-// Brute force approach
+// Brute force approach using Bit Manipulation
 class Solution {
     public int singleNonDuplicate(int[] nums) {
        int sum = 0;
@@ -7,6 +7,22 @@ class Solution {
             sum = sum ^ nums[i];
         }
         return sum;
+    }
+}
+// Time complexity => O(n)
+
+// Better Approach using loops
+class Solution {
+    public int singleNonDuplicate(int[] nums) {
+        for(int i=0;i<=nums.length-1;i = i+2){
+             if(i == nums.length-1){
+                return nums[i];
+            }
+            if(nums[i] != nums[i+1]){
+                return nums[i];
+            }
+        }
+        return nums[0];
     }
 }
 // Time complexity => O(n)
