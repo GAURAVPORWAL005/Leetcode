6aa71f715b04ef7ecec087f891673bf50a07fefa
Gaurav Porwal <53477729+GAURAVPORWAL005@users.noreply.github.com>
2022-06-26 17:29:40 +0530

    Update First and last position of element.java

--- Binary Search/First and last position of element.java
+++ Binary Search/First and last position of element.java
@@ -1,4 +1,37 @@
 // Question => https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/
+
+// Brute force Approach
+// Use Two Pointer Approach
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        int[] ans = {-1,-1};
+        if(nums.length == 1 && nums[0] == target){
+            ans[0] = 0;
+            ans[1] = 0;
+        }
+        
+        // find the starting index
+        for(int i=0;i<nums.length;i++){
+            if(nums[i] == target){
+                ans[0] = i;
+                break;
+            }
+        }
+        
+        // Find the last index
+        for(int j = nums.length-1;j>=0;j--){
+            if(nums[j] == target){
+                ans[1] = j;
+                break;
+            }
+        }
+        return ans;
+    }
+}
+
+// Time complexity => O(n)
+
+// Optimized Approach
 class Solution {
     public int[] searchRange(int[] nums, int target) {
         int[] ans = {-1,-1};
