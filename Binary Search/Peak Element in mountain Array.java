91e32620270afb719f5d6e4187000aaa455b6e5f
Gaurav Porwal <53477729+GAURAVPORWAL005@users.noreply.github.com>
2022-06-26 18:51:25 +0530

    Update Peak Element in mountain Array.java

--- Binary Search/Peak Element in mountain Array.java
+++ Binary Search/Peak Element in mountain Array.java
@@ -15,3 +15,24 @@ class Solution {
 }
 
 // Time complexity => O(n)
+
+// Optimized Approach
+class Solution {
+    public int peakIndexInMountainArray(int[] arr) {
+        int start = 0;
+        int end = arr.length-1;
+        while(start < end){
+            int mid = start + (end - start) / 2;
+            // We are in the decreasing part but check left side
+            if(arr[mid] > arr[mid + 1]){
+                end = mid;
+            }
+            // We are in the ascending part
+            else{
+                start = mid + 1;
+            }
+        }
+        return start;
+    }
+}
+// Time complexity => O(logn)
