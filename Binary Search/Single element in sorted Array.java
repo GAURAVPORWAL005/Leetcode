d334c97847a753819c00aa8b7fa47a503ad29c2b
Gaurav Porwal <53477729+GAURAVPORWAL005@users.noreply.github.com>
2022-06-27 00:27:56 +0530

    Update Single element in sorted Array.java

--- Binary Search/Single element in sorted Array.java
+++ Binary Search/Single element in sorted Array.java
@@ -26,3 +26,31 @@ class Solution {
     }
 }
 // Time complexity => O(n)
+
+// Optimized Approach
+/* XOR with any odd integer let's say 3 gives you 2.
+3 -> 11
+1 -> 01
+XOR of 3 and 1 -> 10
+
+XOR with any even integer let's say 4 gives you 5.
+4 -> 100
+1 - > 001
+XOR of 4 and 1 -> 101*/
+class Solution {
+    public int singleNonDuplicate(int[] nums) {
+        int start = 0;
+        int end = nums.length-2;
+        while(start <= end){
+          int mid = start + (end - start) / 2;
+            if(nums[mid] == nums[mid ^ 1]){
+                start = mid + 1;
+            }
+            else{
+                end = mid - 1;
+            }
+        }
+        return nums[start];
+    }
+}
+// Time complexity => O(logn)
