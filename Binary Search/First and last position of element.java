22bef5f9c02fb130e62f3ec15da937235d032649
Gaurav Porwal <53477729+GAURAVPORWAL005@users.noreply.github.com>
2022-06-26 17:20:01 +0530

    Create First and last position of element.java

---
+++ Binary Search/First and last position of element.java
@@ -0,0 +1,43 @@
+// Question => https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        int[] ans = {-1,-1};
+        int start = 0;
+        int end = nums.length-1;
+        
+        // Check first occurence
+        
+        while(start <= end){
+            int mid = start + (end - start) / 2;
+            if(nums[mid] == target){
+                ans[0] = mid;
+                end = mid - 1;
+            }
+            else if(nums[mid] > target){
+                end = mid - 1;
+            }
+            else{
+                start = mid + 1;
+            }
+        }
+        
+        // Second Occurence
+         start = 0;
+         end = nums.length-1;
+        while(start <= end){
+            int mid = start + (end - start) / 2;
+            if(nums[mid] == target){
+                ans[1] = mid;
+                start = mid + 1;
+            }
+            else if(nums[mid] > target){
+                end = mid - 1;
+            }
+            else{
+                start = mid + 1;
+            }
+        }
+        return ans;
+    }
+}
+// Time complexity => O(logn)
