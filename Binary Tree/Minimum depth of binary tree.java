078d340c5d38333d614dccf3ecdb77a552147ccb
Gaurav Porwal <53477729+GAURAVPORWAL005@users.noreply.github.com>
2022-06-30 17:53:56 +0530

    Create Minimum depth of binary tree.java

---
+++ Binary Tree/Minimum depth of binary tree.java
@@ -0,0 +1,15 @@
+// Quesiton => https://leetcode.com/problems/minimum-depth-of-binary-tree/
+class Solution {
+    public int minDepth(TreeNode root) {
+        if(root == null){
+            return 0;
+        }
+        int left = minDepth(root.left);
+        int right = minDepth(root.right);
+        if(root.left == null || root.right == null){
+            return Math.max(left,right) + 1;
+        }
+        return Math.min(left,right)+1;
+    }
+}
+// Time complexity => O(n)
