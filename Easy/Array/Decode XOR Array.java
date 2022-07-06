9b4a686c8fbc6bfba300a3eeb94ad16621502c91
Gaurav Porwal <53477729+GAURAVPORWAL005@users.noreply.github.com>
2022-07-06 23:48:45 +0530

    Update Decode XOR Array.java

--- Easy/Array/Decode XOR Array.java
+++ Easy/Array/Decode XOR Array.java
@@ -1,4 +1,6 @@
 // Question => https://leetcode.com/problems/decode-xored-array/
+// Given that encoded[i] = arr[i] XOR arr[i + 1]
+        // then arr[i+1] = encoded[i] XOR arr[i].
 class Solution {
     public int[] decode(int[] encoded, int first) {
         int[] ans = new int[encoded.length+1];
