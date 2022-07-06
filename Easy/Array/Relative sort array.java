9bff5d16ceeaa922c11ec7e222ea8f17565de9e6
Gaurav Porwal <53477729+GAURAVPORWAL005@users.noreply.github.com>
2022-07-06 22:31:23 +0530

    Update Relative sort array.java

--- Easy/Array/Relative sort array.java
+++ Easy/Array/Relative sort array.java
@@ -36,5 +36,33 @@ class Solution {
         return ans;
     }
 }
-// Time complexity => o(n2)
+// Time complexity => o(n2logn)
+// Space complexity => O(n2)
+
+// Better Approach
+class Solution {
+    public int[] relativeSortArray(int[] arr1, int[] arr2) {
+        int[] ans = new int[arr1.length];
+        int t=0;
+        for(int i=0;i<arr2.length;i++){
+            for(int j=0;j<arr1.length;j++){
+                if(arr2[i] == arr1[j]){
+                    ans[t++] = arr1[j];
+                }
+            }
+        }
+        Arrays.sort(arr1);
+        Set<Integer> set = new HashSet<>();
+        for(int i=0;i<arr2.length;i++){
+            set.add(arr2[i]);
+        }
+        for(int i=0;i<arr1.length;i++){
+            if(!set.contains(arr1[i])){
+                ans[t++] = arr1[i];
+            }
+        }
+        return ans;
+    }
+}
+// Time complexity => O(n2logn)
 // Space complexity => O(n)
