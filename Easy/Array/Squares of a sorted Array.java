63a661dd2a1d5088b48a87e57da5ae9c888c0594
Gaurav Porwal <53477729+GAURAVPORWAL005@users.noreply.github.com>
2022-07-07 10:33:30 +0530

    Update Squares of a sorted Array.java

--- Easy/Array/Squares of a sorted Array.java
+++ Easy/Array/Squares of a sorted Array.java
@@ -1,4 +1,5 @@
 // Question => https://leetcode.com/problems/squares-of-a-sorted-array/submissions/
+// Brute force Approach
 class Solution {
     public int[] sortedSquares(int[] nums) {
         int[] ans = new int[nums.length];
@@ -10,3 +11,28 @@ class Solution {
     }
 }
 // Time complexity => O(nlogn)
+
+
+// Optimized Approach
+//Using two pointer
+class Solution {
+    public int[] sortedSquares(int[] nums) {
+        int[] ans = new int[nums.length];
+        int start = 0;
+        int end = nums.length-1;
+        int index = nums.length-1;
+        while(start <= end){
+            if(Math.abs(nums[start]) > Math.abs(nums[end])){
+                ans[index] = nums[start] * nums[start];
+                start++;
+            }
+            else{
+                ans[index] = nums[end] * nums[end];
+                end--;
+            }
+            index--;
+        }
+        return ans;
+    }
+}
+// Time c omplexity => O(n)
