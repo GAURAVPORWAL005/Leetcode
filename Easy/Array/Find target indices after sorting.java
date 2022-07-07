a995e9bf3976a42e727db838fd8579ace7ff9081
Gaurav Porwal <53477729+GAURAVPORWAL005@users.noreply.github.com>
2022-07-07 11:48:58 +0530

    Update Find target indices after sorting.java

--- Easy/Array/Find target indices after sorting.java
+++ Easy/Array/Find target indices after sorting.java
@@ -13,3 +13,26 @@ class Solution {
     }
 }
 // Time complexity => O(nlogn)
+
+// Optimized Approach using two pointer
+class Solution {
+    public List<Integer> targetIndices(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length-1;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i] < target){
+                start++;
+            }
+            else if(nums[i] > target){
+                end--;
+            }
+        }
+        List<Integer> list  = new ArrayList<>();
+        while(start<= end){
+            list.add(start);
+            start++;
+        }
+        return list;
+    }
+}
+// Time complexity => O(n)
