cb488ea0c00182f3f33eb3a980e1385b3e3ef33f
Gaurav Porwal <53477729+GAURAVPORWAL005@users.noreply.github.com>
2022-07-07 11:17:07 +0530

    Update Maximum product of two element.java

--- Easy/Array/Maximum product of two element.java
+++ Easy/Array/Maximum product of two element.java
@@ -24,3 +24,24 @@ class Solution {
     }
 }
 // Time complexity => O(logn)
+
+
+// Optimized Approach find max and sec max
+class Solution {
+    public int maxProduct(int[] nums) {
+        int max = Integer.MIN_VALUE;
+        int sec_max = Integer.MIN_VALUE;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i] > max){
+                sec_max = max;
+                max = nums[i];
+                
+            }
+            else if(nums[i] >= sec_max){
+                sec_max = nums[i];
+            }
+        }
+        return ((max-1) * (sec_max-1));
+    }
+}
+// Time complexity => O(n)
