67a034d41c660d9952fe8f9512d777483d3a9ea3
Gaurav Porwal <53477729+GAURAVPORWAL005@users.noreply.github.com>
2022-07-07 10:56:11 +0530

    Update Sort array by parity.java

--- Easy/Array/Sort array by parity.java
+++ Easy/Array/Sort array by parity.java
@@ -20,3 +20,24 @@ class Solution {
 }
 
 // Time complexity => O(n)
+
+
+// using extra space
+class Solution {
+    public int[] sortArrayByParity(int[] nums) {
+        int[] ans = new int[nums.length];
+        int start = 0;
+        int end = nums.length-1;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i] %2 == 0){
+                ans[start++] = nums[i];
+            }
+            else{
+                ans[end--] = nums[i];
+            }
+        }
+        return ans;
+    }
+}
+// Time complexity => O(n)
+// Space complexity => O(n)
