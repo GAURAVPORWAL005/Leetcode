449462dab98863f7bd443744054f57925d61724d
Gaurav Porwal <53477729+GAURAVPORWAL005@users.noreply.github.com>
2022-07-14 11:31:50 +0530

    Update Longest common prefix.java

--- String/Longest common prefix.java
+++ String/Longest common prefix.java
@@ -4,6 +4,31 @@ https://leetcode.com/problems/longest-common-prefix/
 
 class Solution {
     public String longestCommonPrefix(String[] strs) {
+        if(strs == null || strs.length == 0){
+            return "";
+        }
+        Arrays.sort(strs);
+        String first = strs[0];
+        String last = strs[strs.length-1];
+        int c = 0;
+        while(c < first.length()){
+            if(first.charAt(c) != last.charAt(c)){
+                break;
+            }
+            else{
+                c++;
+            }
+        }
+              return c == 0 ? "": first.substring(0,c);
+    }
+}
+
+// time complexity => O(nlogn)
+
+// brute force approach
+
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
         String prefix = strs[0];
         for(int i=1;i<strs.length;i++){
             while(strs[i].indexOf(prefix) != 0){
