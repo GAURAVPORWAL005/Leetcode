224f680466ccf882085035a8031678b89f7a4070
Gaurav Porwal <53477729+GAURAVPORWAL005@users.noreply.github.com>
2022-07-28 11:24:01 +0530

    Create Valid Anagram.java

---
+++ String/Valid Anagram.java
@@ -0,0 +1,13 @@
+https://leetcode.com/problems/valid-anagram/
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        char[] sArray = s.toCharArray();
+        Arrays.sort(sArray);
+        String first = new String(sArray);
+        char[] tArray = t.toCharArray();
+        Arrays.sort(tArray);
+        String second  = new String(tArray);
+        return first.equals(second);
+    }
+}
+// time complexity => O(nlogn)
